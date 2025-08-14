6250aee5667694bbdf3b18f70eed1b8f5a189167
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-14 22:28:03 +0530

    Update Step1Aadhaar.jsx

--- frontend/src/UdyamForm/Step1Aadhaar.jsx
+++ frontend/src/UdyamForm/Step1Aadhaar.jsx
@@ -24,7 +24,7 @@ const Step1Aadhaar = () => {
     setLoading(true);
 
     try {
-      const res = await fetch('https://udyamr1.onrender.com/api/generate-otp', {
+      const res = await fetch('https://udyamresgister-1.onrender.com/api/generate-otp', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ aadhaar: formData.aadhaar })
@@ -52,7 +52,7 @@ const Step1Aadhaar = () => {
 
     setLoading(true);
     try {
-      const res = await fetch('https://udyamr1.onrender.com/api/verify-otp', {
+      const res = await fetch('https://udyamresgister-1.onrender.com/api/verify-otp', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
